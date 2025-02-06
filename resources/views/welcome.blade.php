b078d2cba7366a07de8b6bf52a88723c43679aab
VALERIACARMONA\valec <v.carmona.isw@unipolidgo.edu.mx>
2025-02-06 13:07:08 -0600

    imagenesvale

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,16 @@
 <div id="carouselExample" class="carousel slide">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="Images/caruselImage1.jpg" class="d-block w-100" alt="...">
+        <img src="Images/carimage1.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="Images/caruselImage1.jpg" class="d-block w-100" alt="...">
+        <img src="Images/carimage3.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="Images/caruselImage1.jpg" class="d-block w-100" alt="...">
+        <img src="Images/carimage2.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="Images/carimage4.png" class="d-block w-100" alt="...">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
