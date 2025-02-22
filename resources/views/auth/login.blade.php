7fa6e6b0cd49be9823be6359a86812c1e52e90c3
Fer8-8 <fernandonajerarascon@gmail.com>
2025-02-22 17:59:36 -0600

    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,109 +1,72 @@
 @extends('layouts.app')
 
+@section('head')
+<style>
+    body {
+        background: url('Images/background.png') no-repeat center center fixed;
+        background-size: cover;
+    }
+    .login-container {
+        width: 80vh;
+        background: rgba(255, 255, 255, 0.7);
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    .logo {
+        position: absolute;
+        display: block;
+        margin: 0 auto 15px;
+        width: 80px;
+        top: -60px;
+        transform: scale(2.5);
+    }
+    .box{
+        position: relative;
+    }
+    .login-btn{
+        height: 30px;
+        font-size: 12px;
+        flex: 1;
+    }
+    .f-btn{
+        background-color: #1877F2 ;
+    }
+</style>
+@endsection
+
 @section('content')
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<div class="container d-flex justify-content-center align-items-center vh-100">
+    <div class="login-container text-center px-5">
+        <div class="d-flex justify-content-center box my-3">  
+            <img src="Images/Logo.png" alt="RaiDur Logo" class="logo">
         </div>
+        <form class="mt-5">
+            <div class="mb-3 text-start fw-bold">
+                <label for="mail" class="form-label">Correo electronico:</label>
+                <input id="mail" type="email" class="form-control border border-black" required>
+            </div>
+            <div class="mb-3 text-start fw-bold">
+                <label for="mail" class="form-label">Contraseña:</label>
+                <input type="password" class="form-control border border-black" required>
+            </div>
+            <div class="d-flex justify-content-between align-items-center mb-3">
+                <div>
+                    <input type="checkbox" id="remember">
+                    <label for="remember">Recuérdame</label>
+                </div>
+                <a href="#">Olvidé mi contraseña</a>
+            </div>
+            <button type="submit" class="btn btn-primary px-4 fw-bold text-shadow-sm border border-black">ACCEDER</button>
+        </form>
+        <div class="d-flex flex-row mt-4 justify-content-between">
+            <button class="btn p-1 btn-dark login-btn flex-growth-1">Continue with Apple</button>
+            <button class="btn p-1 btn-light mx-3 login-btn flex-growth-1">Continue with Google</button>
+            <button class="btn p-1 btn-primary login-btn text-white flex-growth-1 f-btn">Continue with Facebook</button>
+        </div>
+        <p class="mt-3">¿No tienes cuenta? <a href="#">Regístrate aquí</a></p>
     </div>
 </div>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,72 +22,11 @@
     <!-- Google Fonts: Saira -->
     <link href="https://fonts.googleapis.com/css2?family=Saira:wght@300;400;600;700&display=swap" rel="stylesheet">
 
+    @yield('head')
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-black shadow-sm d-flex justify-content-between">
-            <div class="container-fluid mx-0">
-
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
-                    <a class="navbar-brand" href="{{ route('welcome') }}">
-                        <img class='mx-2' src="/Images/RaiDurLogo.jpg" alt="Logo" width="auto" height="40">
-                        <img src="/Images/RaiDurLogoText.jpg" alt="Logo" width="auto" height="40">
-                      </a>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto gap-5">
-
-                    <li class="nav-item">
-                        <a href="" class="nav-link text-white">Servicios</a>
-                    </li>
-                    <li class="nav-item"><a href="" class="nav-link text-white saira-font">Nosotros</a></li>
-                    <li class="nav-item"><a href="" class="nav-link text-white saira-font">FAQs</a></li>
-                    @guest
-                        <a href="{{ route('login') }}" type="button" class="btn btn-primary px-2 mx-3"><strong class="text-shadow-sm">INICIAR SESIÓN </strong></a>
-
-                    @else
-                        <li class="nav-item">asdfajsfd</li>
-                    @endguest
-                    <!-- Authentication Links -->
-                    <!---
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
-
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                    --->
-                </ul>
-            </div>
-        </nav>
-
         <main class="py-0">
             @yield('content')
         </main>
