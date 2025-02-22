a80f10328f99bdb400733aa3e8e447fbab5ed762
VALERIACARMONA\valec <v.carmona.isw@unipolidgo.edu.mx>
2025-02-21 23:08:37 -0600

    iconos

---
+++ resources/views/calentadores.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Calentadores</title>
+</head>
+<body>
+    <h1>Bienvenido a la Página de Calentadores</h1>
+    <p>Esta es la página de información sobre cisternas.</p>
+</body>
+</html>
