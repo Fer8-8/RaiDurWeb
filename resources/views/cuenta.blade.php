e81f6e80345d4bcb7e37eb49339a68a22e5fd4cf
PEACHYTWILIGHT\nyan_ <m.andrade.isw@unipolidgo.edu.mx>
2025-03-28 21:56:58 -0600

    cueenta

--- resources/views/cuenta.blade.php
+++ resources/views/cuenta.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <div class="container p-6">
     <div class="bg-white shadow-md rounded-lg p-6">
-<<<<<<< HEAD
         <h2 class="text-xl font-bold flex items-center pt-5">
             <i class="fa fa-user-circle-o" aria-hidden="true"></i>Eduardo
         </h2>
@@ -14,66 +13,36 @@
                 <h3 class="font-semibold d-inline text-gray-700 mt-6">Dirección de envío:</h3>
                 <a href="#" class="text-info hover:underline absolute top-2 right-2">Cambiar</a>
             </div>
-=======
-        <h2 class="text-xl font-bold flex items-center">
-            <i class="fas fa-user-circle mr-2"></i> Eduardo
-        </h2>
-
-        <!-- Card: Dirección de envío -->
-        <div class="bg-white shadow-lg rounded-x50 p-5 mt-4 border relative">
-            <a href="#" class="text-blue-500 hover:underline absolute top-2 right-2">Cambiar</a>
-            <h3 class="font-semibold text-gray-700 mt-6">Dirección de envío:</h3>
->>>>>>> c4cca9d9fcd4a1b9a34a80ae09f6daea08e21b68
             <p class="text-gray-600">Calle Amanecer #412, Jardines de Durango, Victoria de Durango, Durango, 12345, México.</p>
         </div>
 
         <!-- Card: Método de pago -->
-<<<<<<< HEAD
         <div class="bg-white shadow-lg rounded-5 py-3 px-5 mt-4 border relative">
             <div class="d-flex justify-content-between align-items-center">
                 <h3 class="font-semibold text-gray-700 mt-6">Método de pago y facturación:</h3>
                 <a href="#" class="text-info hover:underline absolute top-2 right-2">Cambiar</a>
             </div>
-=======
-        <div class="bg-white shadow-lg rounded-x50  p-5 mt-4 border relative">
-            <a href="#" class="text-blue-500 hover:underline absolute top-2 right-2">Cambiar</a>
-            <h3 class="font-semibold text-gray-700 mt-6">Método de pago y facturación:</h3>
->>>>>>> c4cca9d9fcd4a1b9a34a80ae09f6daea08e21b68
             <p class="text-gray-600">Visa 4516</p>
         </div>
 
         <!-- Card: Historial de pedidos -->
-<<<<<<< HEAD
         <div class="bg-white shadow-lg rounded-5 py-3 px-5 mt-4 border relative">
             <div class="d-flex justify-content-between align-items-center">
                 <h3 class="font-semibold text-gray-700 mt-6">Historial de pedidos</h3>
                 <a href="#" class="text-info hover:underline absolute top-2 right-2">Ver</a>
             </div>
-=======
-        <div class="bg-white shadow-lg rounded-x50 p-5 mt-4 border relative">
-            <a href="#" class="text-blue-500 hover:underline absolute top-2 right-2">Ver</a>
-            <h3 class="font-semibold text-gray-700 mt-6">Historial de pedidos</h3>
->>>>>>> c4cca9d9fcd4a1b9a34a80ae09f6daea08e21b68
             <p class="text-gray-600">Calle Amanecer #412, Jardines de Durango, Victoria de Durango, Durango, 12345, México.</p>
         </div>
 
         <!-- Card: Servicio al cliente -->
-<<<<<<< HEAD
         <div class="bg-white shadow-lg rounded-5 py-3 px-5 mt-4 border relative">
             <div class="d-flex justify-content-between align-items-center">
                 
                 <h3 class="font-semibold text-gray-700 mt-6">Servicio al cliente</h3>
                 <a href="#" class="text-info hover:underline absolute top-2 right-2">Contáctanos</a>
             </div>
-=======
-        <div class="bg-white shadow-lg rounded-x50 p-5 mt-4 border relative">
-            <a href="#" class="text-blue-500 hover:underline absolute top-2 right-2">Contáctanos</a>
-            <h3 class="font-semibold text-gray-700 mt-6">Servicio al cliente</h3>
->>>>>>> c4cca9d9fcd4a1b9a34a80ae09f6daea08e21b68
         </div>
     </div>
 </div>
-
-
-   
 @endsection
+   
